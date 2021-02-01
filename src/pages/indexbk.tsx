b367f3fd80ad2d5a563061f56d040605ec94f422
hori7 <horiuchi.kouta00@gmail.com>
2021-02-01 23:11:03 +0900

    fix style

---
+++ src/pages/indexbk.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Header from '../components/header'
+import ExtLink from '../components/ext-link'
+import Features from '../components/features'
+import GitHub from '../components/svgs/github'
+import sharedStyles from '../styles/shared.module.css'
+
+export default () => (
+  <>
+    <Header titlePre="Home" />
+    <div className={sharedStyles.layout}>
+      {/* <img
+        src="/vercel-and-notion.png"
+        height="85"
+        width="250"
+        alt="Vercel + Notion"
+      /> */}
+      {/* <h1>My Notion Blog</h1> */}
+      {/* <Features /> */}
+    </div>
+  </>
+)
